f7133f8df92793856a718b55058dc879353be3a0
temple <utemple169@gmail.com>
2022-06-23 16:18:28 +0100

    updated django_blog

--- I4G013717YJH/blog/views.py
+++ I4G013717YJH/blog/views.py
@@ -11,16 +11,20 @@ from .models import Post
 # Create your views here.
 class PostListView(ListView):
     model=Post
+    template_name= "blog\post_list.html"
 class PostCreateView(CreateView):
     model=Post
     fields="__all__"
     success_url=reverse_lazy("blog:all")
+    template_name="blog\post_form.html"
 class PostDetailView(DetailView):
     model=Post
+    template_name="blog\post_detail.html"
 class PostUpdateView(UpdateView):
     model=Post
-    feilds="__all__"
+    fields="__all__"
     success_url=reverse_lazy("blog:all")
+    template_name="blog\post_form.html"
 class PostDeleteView(DeleteView):
     model=Post
     fields="__all__"
